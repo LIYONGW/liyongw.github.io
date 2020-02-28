027c16f7823670ce992baa0ca0d292d147524a1d
LIYONG <liyongwong1999@gmail.com>
2020-02-28 17:09:21 -0500

    d

---
+++ whoiam-script.js
@@ -0,0 +1,341 @@
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "14px Arial";
+ctx.fillText("WHO_I_AM", 525, 25);
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("SELF_PORTRAIT", 85, 300);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(10, 10);
+ctx.lineTo(210, 10);
+ctx.lineTo(210, 320);
+ctx.lineTo(10, 320);
+ctx.lineTo(10,10)
+ctx.stroke();
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("FULL_NAME", 290, 155);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "10px Arial";
+ctx.fillText("黄 力 扬", 225, 120);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "10px Arial";
+ctx.fillText("LIYONG_WONG", 225, 105);
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "10px Arial";
+ctx.fillText("リ ヨ ン ・ ウ ォ ン グ", 225, 135);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(210, 80);
+ctx.lineTo(413, 80);
+ctx.lineTo(413, 170);
+ctx.lineTo(210, 170);
+ctx.lineTo(210, 80);
+ctx.stroke();
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("NICK_NAME", 540, 215);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(413, 80);
+ctx.lineTo(602, 80);
+ctx.lineTo(602, 230);
+ctx.lineTo(526, 230);
+ctx.lineTo(413, 170);
+ctx.lineTo(413, 80);
+ctx.stroke();
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("HOBBIES", 295, 300);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(210, 170);
+ctx.lineTo(413, 170);
+ctx.lineTo(329, 261);
+ctx.lineTo(444, 320);
+ctx.lineTo(210, 320);
+ctx.lineTo(210, 170);
+ctx.stroke();
+
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("BIRTH_DAY", 410, 290);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(329, 261);
+ctx.lineTo(372, 215);
+ctx.lineTo(486, 275);
+ctx.lineTo(444, 320);
+ctx.stroke();
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("BIRTH_PLACE", 445, 245);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(413, 170);
+ctx.lineTo(526, 230);
+ctx.lineTo(486, 275);
+ctx.lineTo(372, 215)
+ctx.stroke();
+
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("MY_FAMILY", 545, 360);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(526, 230);
+ctx.lineTo(602, 230);
+ctx.lineTo(602, 402);
+ctx.lineTo(444, 320);
+ctx.lineTo(526, 230);
+ctx.stroke();
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("PROUD_OF", 185, 480);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(10, 320);
+ctx.lineTo(240, 320);
+ctx.lineTo(240, 553);
+ctx.lineTo(10, 320);
+ctx.stroke();
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("ADMIRE_PERSON", 270, 390);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(240, 320);
+ctx.lineTo(403, 402);
+ctx.lineTo(240, 402);
+ctx.lineTo(240, 320);
+ctx.stroke();
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("LIVED_PLACES", 440, 390);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(240, 320);
+ctx.lineTo(403, 402);
+ctx.lineTo(603, 402);
+ctx.lineTo(445, 320);
+ctx.stroke();
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("I_NEVER", 35, 540);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(10, 320);
+ctx.lineTo(95, 405);
+ctx.lineTo(95, 553);
+ctx.lineTo(10, 553);
+ctx.lineTo(10, 320);
+ctx.stroke();
+
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("FREE_TIME", 135, 540);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(95, 405);
+ctx.lineTo(240, 553);
+ctx.lineTo(95, 553);
+ctx.lineTo(95, 405);
+ctx.stroke();
+
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("RECENT_MEM", 97, 630);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(10, 553);
+ctx.lineTo(240, 553);
+ctx.lineTo(125, 680);
+ctx.lineTo(10, 553);
+ctx.stroke();
+
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("GOOD_TEACHER", 35, 750);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(10, 553);
+ctx.lineTo(125, 680);
+ctx.lineTo(125, 765);
+ctx.lineTo(10, 765);
+ctx.lineTo(10, 553);
+ctx.stroke();
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("CHILD_MEMORY", 190, 750);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(240, 553);
+ctx.lineTo(125, 680);
+ctx.lineTo(125, 765);
+ctx.lineTo(421, 765);
+ctx.stroke();
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "6px Arial";
+ctx.fillText("PETS", 245, 470);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(240, 402);
+ctx.lineTo(240, 402);
+ctx.lineTo(403, 402);
+ctx.stroke();
+
+
+
+
+
+
+
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(420, 583.5, 181.5, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(421, 765);
+ctx.lineTo(421, 402);
+ctx.stroke();
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(238, 583);
+ctx.lineTo(601, 583);
+ctx.stroke();
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(290, 710);
+ctx.lineTo(550, 458);
+ctx.stroke();
+
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(290, 458);
+ctx.lineTo(550, 710);
+ctx.stroke();
+
+
